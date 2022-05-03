5b21272e078fce47cecf5ff26121311865f76b07
Julien <gerard.julien2000@orange.fr>
2022-05-03 11:45:08 +0200

    AddTask.js : form linked to the reducer, AddTask done

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useStateValue } from "./StateProvider";
 
 function AddTask() {
+  const [{ todolist }, dispatch] = useStateValue();
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_TASK",
+      item: {
+        name: name,
+        id: 2,
+        completed: false,
+      },
+    });
+    setName("");
+    console.log({ todolist });
+  };
+
   return (
     <div className="addTask">
-      <form className="addTask_form">
+      <form className="addTask_form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="addTask_text"
           placeholder="Write a new task here"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           required
         />
         <button type="submit" className="addTask_submit">
